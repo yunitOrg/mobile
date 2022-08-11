4e2c551dc297f1ea9b923a5a9f5b1aee39c22043
hanjunpeng <1358271059@qq.com>
2022-08-11 18:19:14 +0800

    提交组件

--- src/components/IHeadList.vue
+++ src/components/IHeadList.vue
@@ -9,7 +9,40 @@
   :id="moduleObject.id" 
   :idm-ctrl-id="moduleObject.id" 
   >
-    fds
+    <div class="idm-iheadlist">
+      <div class="iheadlist-ul">
+        <li v-for="(item, index) in list" :key="index">
+          <div class="iheadlist-img">
+            <img :src="item.img" alt="">
+          </div>
+          <div class="iheadlist-right">
+            <div class="iheadlist-center">
+              <div class="iheadlist-line">
+                <span class="iheadlist-author">{{item.author}}</span>
+                <svg
+                  class="idm_button_svg_icon idm-icon-settingOutline"
+                  aria-hidden="true"
+                >
+                  <use :xlink:href="`#icon-settingOutline`"></use>
+                </svg>
+                <span class="color">{{item.tip1}}</span>
+                <span class="color">{{item.tip2}}</span>
+                <span class="color1">{{item.type === 1 && '已激活'}}</span>
+              </div>
+              <span>{{item.desc}}</span>
+            </div>
+            <div class="iheadlist-icon">
+              <svg
+                class="idm_button_svg_icon idm-icon-settingOutline"
+                aria-hidden="true"
+              >
+                <use :xlink:href="`#icon-settingOutline`"></use>
+              </svg>
+            </div>
+          </div>
+        </li>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -18,6 +51,24 @@ export default {
   name: 'IHeadList',
   data () {
     return {
+      list: [
+        {
+          author: '宋尚朴',
+          img: 'http://116.236.111.158:8086/DreamWeb/resource/img/body-bg-shanghai.png',
+          desc: '中共上海市测试三支部',
+          tip1: '书记',
+          tip2: '组织部',
+          type: 1
+        },
+        {
+          author: '宋尚朴',
+          img: 'http://116.236.111.158:8086/DreamWeb/resource/img/body-bg-shanghai.png',
+          desc: '中共上海市测试三支部',
+          tip1: '书记',
+          tip2: '组织部',
+          type: 1
+        }
+      ],
       moduleObject:{},
       propData:this.$root.propData.compositeAttr||{
           fontContent:"Hello Word"
@@ -39,3 +90,69 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.idm-iheadlist{
+  .iheadlist-ul{
+    margin: 0 10px;
+    border-radius: 10px;
+    background: #fff;
+    box-shadow: 0px  0px 20px 0px #ccc;
+    li{
+      list-style: none;
+      padding: 0 20px;
+      display: flex;
+      align-items: center;
+    }
+    li:last-child .iheadlist-right{
+      border: 0;
+    }
+    .idm_button_svg_icon{
+      width: 20px;
+      height: 20px;
+      vertical-align: middle;
+      margin-right: 5px;
+    }
+    .iheadlist-img{
+      width: 40px;
+      height: 40px;
+      margin-right: 10px;
+      img{
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+      }
+    }
+    .iheadlist-right{
+      padding: 20px 0;
+      width: calc(100% - 50px);
+      display: flex;
+      align-items: center;
+      border-bottom: 1px solid #ccc;
+      .iheadlist-center{
+        display: flex;
+        flex-wrap: wrap;
+        .iheadlist-line{
+          padding-bottom: 5px;
+          span{
+            margin-right: 5px;
+          }
+        }
+        .color{
+          padding: 3px 5px;
+          border-radius: 3px;
+          color: #fff;
+          background: #F7B503;
+        }
+        .color1{
+          padding: 3px 5px;
+          border-radius: 3px;
+          color: #fff;
+          background: #44D7B6;
+        }
+      }
+    }
+  }
+}
+</style>
+
--- src/components/IdeaList.vue
+++ src/components/IdeaList.vue
@@ -9,7 +9,23 @@
   :id="moduleObject.id" 
   :idm-ctrl-id="moduleObject.id" 
   >
-    fds
+    <div class="idm-idealist">
+      <div class="idealist-ul">
+        <li v-for="(item, index) in list" :key="index">
+          <div class="idealist-img">
+            <img src="http://116.236.111.158:8086/DreamWeb/resource/img/body-bg-shanghai.png" alt="">
+          </div>
+          <div class="idealist-content">
+            <div class="idealist-title">
+              <b>{{item.title}}</b>
+              <span>{{item.time}}</span>
+            </div>
+            <span>{{item.desc}}</span>
+          </div>
+          <div class="idealist-right">{{item.type === 1 ? '待回复' : '已回复'}}</div>
+        </li>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -18,6 +34,22 @@ export default {
   name: 'IHeadList',
   data () {
     return {
+      list: [
+        {
+          headImg: 'http://116.236.111.158:8086/DreamWeb/resource/img/body-bg-shanghai.png',
+          title: '白继伟',
+          desc: "书记，我的思想汇报有误",
+          time: '2021-08-25',
+          type: 1
+        },
+        {
+          headImg: 'http://116.236.111.158:8086/DreamWeb/resource/img/body-bg-shanghai.png',
+          title: '白继伟',
+          desc: "书记，我的思想汇报有误",
+          time: '2021-08-25',
+          type: 1
+        }
+      ],
       moduleObject:{},
       propData:this.$root.propData.compositeAttr||{
           fontContent:"Hello Word"
@@ -39,3 +71,42 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.idm-idealist{
+  .idealist-ul{
+    padding: 0 10px;
+    li{
+      list-style: none;
+      display: flex;
+      padding: 20px;
+      align-items: center;
+      border-radius: 5px;
+      box-shadow: 0px  0px 20px 0px #ccc;
+      .idealist-img{
+        width: 40px;
+        height: 40px;
+        margin-right: 10px;
+        img{
+          width: 100%;
+          height: 100%;
+          border-radius: 50%;
+        }
+      }
+      .idealist-content{
+        flex: 1;
+        .idealist-title{
+          padding-bottom: 5px;
+        }
+        b{
+          font-size: 16px;
+          padding-right: 30px;
+        }
+      }
+    }
+    li+li{
+      margin-top: 20px;
+    }
+  }
+}
+</style>
