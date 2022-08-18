483f67d8046386963c588f2a8bbd1a3d08f11eec
hanjunpeng <1358271059@qq.com>
2022-08-18 17:31:51 +0800

    提交代码

--- src/components/ITags.vue
+++ src/components/ITags.vue
@@ -41,18 +41,6 @@ export default {
           {
             key: '3',
             tab: '党费',
-          },
-          {
-            key: '4',
-            tab: '党费',
-          },
-          {
-            key: '5',
-            tab: '党费',
-          },
-          {
-            key: '6',
-            tab: '党费',
           }
         ],
         boxWidth: '300px',
@@ -107,6 +95,26 @@ export default {
           item.defaultActiveKey =  item.defaultActiveKey ? false : (item.defaultActiveKey === 'undefined' ? true : !item.defaultActiveKey)
         }
       })
+      // 发送消息给组件
+      this.sendBroadcastMessage({
+        type: 'chooseTag',
+        rangeModule: this.propData.triggerComponents && this.propData.triggerComponents.map(el => el.moduleId),
+        message: this.tablist.filter(item => item.defaultActiveKey)
+      })
+    },
+     /**
+     * 组件通信：发送消息的方法
+     * @param {
+     *  type:"自己定义的，统一规定的type：linkageResult（组件联动传结果值）、linkageDemand（组件联动传需求值）、linkageReload（联动组件重新加载）
+     * 、linkageOpenDialog（打开弹窗）、linkageCloseDialog（关闭弹窗）、linkageShowModule（显示组件）、linkageHideModule（隐藏组件）、linkageResetDefaultValue（重置默认值）",
+     *  message:{实际的消息对象},
+     *  rangeModule:"为空发送给全部，根据配置的属性中设定的值（值的内容是组件的packageid数组），不取子表的，比如直接 this.$root.propData.compositeAttr["attrKey"]（注意attrKey是属性中定义的bindKey）,这里的格式为：['1','2']"",
+     *  className:"指定的组件类型，比如只给待办组件发送，然后再去过滤上面的值"
+     *  globalSend:如果为true则全站发送消息，注意全站rangeModule是无效的，只有className才有效，默认为false
+     * } object 
+     */
+    sendBroadcastMessage(object) {
+        window.IDM.broadcast && window.IDM.broadcast.send(object);
     },
     convertTitleStyleObject () {
       let styleObject = {};
