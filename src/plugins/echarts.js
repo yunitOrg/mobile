0f794a7d6bdc60a323cd9b8b3b9b85c6622d1bd5
周子正 <ZeroCLemonTea@163.com>
2022-09-08 18:50:38 +0800

    更新饼状图组件

--- src/plugins/echarts.js
+++ src/plugins/echarts.js
@@ -4,7 +4,7 @@ import * as echarts from 'echarts/core';
 // 引入柱状图图表，图表后缀都为 Chart
 import { PieChart } from 'echarts/charts';
 // 引入提示框，标题，直角坐标系，数据集，内置数据转换器组件，组件后缀都为 Component
-import { LegendComponent, TooltipComponent } from 'echarts/components';
+import { LegendComponent, TooltipComponent, TitleComponent } from 'echarts/components';
 // 标签自动布局，全局过渡动画等特性
 import { UniversalTransition } from 'echarts/features';
 // 引入 Canvas 渲染器，注意引入 CanvasRenderer 或者 SVGRenderer 是必须的一步
@@ -13,4 +13,11 @@ import { CanvasRenderer } from 'echarts/renderers';
 // 将echarts挂载到Vue原型上
 Vue.prototype.$echarts = echarts;
 // 注册必须的组件
-echarts.use([PieChart, LegendComponent, TooltipComponent, UniversalTransition, CanvasRenderer]);
+echarts.use([
+  PieChart,
+  LegendComponent,
+  TooltipComponent,
+  TitleComponent,
+  UniversalTransition,
+  CanvasRenderer
+]);
