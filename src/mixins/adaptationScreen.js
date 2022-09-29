5ea48f8981d5c3076d63f033ff2ceeb73b44cc88
yangyk520 <237136432@qq.com>
2022-09-29 15:46:50 +0800

    添加头部组件

--- src/mixins/adaptationScreen.js
+++ src/mixins/adaptationScreen.js
@@ -11,21 +11,19 @@ export default {
          * @param {*} ratio    配置的适配比例
          * @returns  计算后的尺寸 type Number
          */
-        getAdaptiveSize(initSize, ratio = 0) {
+        getAdaptiveSize(initSize, ratio = 0, is_img) {
             if (this.moduleObject.env == 'develop' && !this.pageWidth) return initSize
             initSize = Number(initSize)
             const WINDOW_WIDTH = this.pageWidth || window.innerWidth // 当前屏幕宽度
             const BASE_SIZE = Number(this.propData.baseSize) || 414 // 配置屏幕基准值宽度
             const ADAPTATION_RATIO = ratio || Number(this.propData.adaptationRatio) || 1.2 // 配置适配比例
-            const FINAL_RESULT = initSize * ((WINDOW_WIDTH / BASE_SIZE - 1) * (ADAPTATION_RATIO - 1) + 1)
+            let FINAL_RESULT
+            if ( is_img ) {
+                FINAL_RESULT = initSize * ((WINDOW_WIDTH / BASE_SIZE - 1) * (ADAPTATION_RATIO - 0.5) + 1)
+            } else {
+                FINAL_RESULT = initSize * ((WINDOW_WIDTH / BASE_SIZE - 1) * (ADAPTATION_RATIO - 1) + 1)
+            }
             if (!WINDOW_WIDTH || !BASE_SIZE || !ADAPTATION_RATIO || window.isNaN(FINAL_RESULT)) {
-                // console.error(`
-                //   In ${this.$options.name} Calculate's params error
-                //   WINDOW_WIDTH      ---->  ${WINDOW_WIDTH}
-                //   BASE_SIZE         ---->  ${BASE_SIZE}
-                //   ADAPTATION_RATIO  ---->  ${ADAPTATION_RATIO}
-                //   FINAL_RESULT      ---->  ${FINAL_RESULT}
-                // `)
                 return initSize
             }
             return Math.round(FINAL_RESULT)
