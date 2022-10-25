74a868be836d9868edbd379fd2e1d2b7bdeaf647
karmaangel <asdfgh1623@126.com>
2022-10-25 17:15:31 +0800

    评论组件更新
    评论组件更新

--- src/components/ICommentPublish.vue
+++ src/components/ICommentPublish.vue
@@ -161,7 +161,29 @@ export default {
       console.log("styleObject:");
 
       window.IDM.setStyleToPageHead(this.moduleObject.id + " .i-comment-publish-content", styleObject);
-    }
+    },
+    /**
+     * 组件通信：接收消息的方法
+     * @param {
+     *  type:"发送消息的时候定义的类型，这里可以自己用来要具体做什么，统一规定的type：linkageResult（组件联动传结果值）、linkageDemand（组件联动传需求值）、linkageReload（联动组件重新加载）
+     * 、linkageOpenDialog（打开弹窗）、linkageCloseDialog（关闭弹窗）、linkageShowModule（显示组件）、linkageHideModule（隐藏组件）、linkageResetDefaultValue（重置默认值）"
+     *  message:{发送的时候传输的消息对象数据}
+     *  messageKey:"消息数据的key值，代表数据类型是什么，常用于表单交互上，比如通过这个key判断是什么数据"
+     *  isAcross:如果为true则代表发送来源是其他页面的组件，默认为false
+     * } object
+     */
+    receiveBroadcastMessage(messageObject) {
+      console.log("组件收到消息", messageObject);
+      console.log(messageObject);
+      let that = this;
+      switch (messageObject.type) {
+        case "replayComment":
+          that.$nextTick(()=>{
+            this.$refs.input.focus();
+          });
+          break;
+      }
+    },
   }
 }
 </script>
