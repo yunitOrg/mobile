f874671d3f2abaa38eca87212b214c60492e2539
hanjunpeng <1358271059@qq.com>
2022-11-02 17:33:09 +0800

    提交代码

--- src/components/IntegralDetal.vue
+++ src/components/IntegralDetal.vue
@@ -126,35 +126,20 @@ export default{
     },
     initData () {
       let that = this;
-      const customInterfaceUrl = '/ctrl/dataSource/getDatas';
       if (this.moduleObject.env == "production") {
-        this.isLoading = true
-        this.propData.dataSource &&
-          IDM.http
-            .post(
-              customInterfaceUrl,
-              {
-                id: this.propData.dataSource && this.propData.dataSource.value,
-              },
-              {
-                headers: {
-                  "Content-Type": "application/json;charset=UTF-8",
-                },
-              }
-            )
-            .then((res) => {
-              if (res.status == 200 && res.data.code == 200) {
-                let obj = (res.data || {}).data
-                that.pageData = obj;
-                let tem = that.propData.liField;
-                that.list = obj[tem]
-              } else {
-                IDM.message.error(res.message);
-              }
-            })
-            .finally(() => {
-              this.isLoading = false
-            })
+        this.isLoading = true;
+        if (this.propData.dataSource) {
+          IDM.datasource.request(this.propData.dataSource[0]?.id, {
+            moduleObject: this.moduleObject
+          }, (data) => {
+            this.isLoading = false;
+            if (data) {
+              that.pageData = data;
+              let tem = that.propData.liField;
+              that.list = data[tem]
+            }
+          })
+        }
       }
     },
     converStyleObj () {
